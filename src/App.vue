8bd9f83ae95069e8351ee5deb4c487b618e8b967
Yuliia <yuliia.yaremko@ivvysoft.com>
2021-07-27 16:02:08 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <PokemonApp msg="Welcome Pokemon application"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonApp from './components/PokemonApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokemonApp
   }
 }
 </script>
---
+++ src/components/PokemonApp.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="pokemons">
+    <div class="container">
+      <h2>{{ heading }}</h2>
+
+      <div class="pokemons__content">
+        <ul class="pokemons__list">
+          <li
+            class="pokemons__item"
+            v-for="(pokemon, index) in pokemons"
+            v-bind:key="index"
+          >
+            {{ pokemon.name }}
+            {{ pokemon.type }}
+          </li>
+        </ul>
+
+        <div class="pokemons__chosen">Here should be a chosen Pokemon</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonApp",
+  // props: {
+  //   msg: String
+  // },
+
+  data: function () {
+    return {
+      heading: "Pokedex",
+      pokemons: [
+        { name: "pikachu", type: "fire" },
+
+        { name: "chobakabra", type: "fire" },
+
+        { name: "spider", type: "iron" },
+
+        { name: "clablah", type: "stone" },
+      ],
+    };
+  },
+
+  // methods: {
+  // getCharachers(id) {
+  //   fetch(`https://swapi.dev/api/people/${id}`, {
+  //     method: "Get",
+  //   })
+  //     .then((response) => response.json())
+  //     .then((json) => (this.character = json));
+  // },
+};
+</script>
+
+<style scoped lang="scss">
+*,
+::after,
+::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  margin: 0 10%;
+}
+
+@mixin items-align($display, $row, $horizontal, $vertical) {
+  display: $display;
+  flex-direction: $row;
+  justify-content: $horizontal;
+  align-items: $vertical;
+}
+
+.pokemons {
+  &__content {
+    @include items-align(flex, row, space-between, center);
+  }
+
+  &__list {
+    @include items-align(grid, unset, space-between, center);
+    grid-template-columns: repeat(3, 30%);
+    flex-basis: 60%;
+    border: 1px solid blue;
+  }
+
+  &__chosen {
+    flex-basis: 35%;
+    border: 1px solid red;
+  }
+}
+</style>
